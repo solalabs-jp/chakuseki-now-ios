41fd0932a4d2dc010d82eb194170000c5926261f
takuya <219540123+suzuki248@ users.noreply.github.com>
2026-04-22 12:02:31 +0900

    Initial Commit

---
+++ chakuseki-now-macTests/chakuseki_now_macTests.swift
@@ -0,0 +1,17 @@
+//
+//  chakuseki_now_macTests.swift
+//  chakuseki-now-macTests
+//
+//  Created by 鈴木拓也 on 2026/04/22.
+//
+
+import Testing
+@testable import chakuseki_now_mac
+
+struct chakuseki_now_macTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ chakuseki-now-macUITests/chakuseki_now_macUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  chakuseki_now_macUITestsLaunchTests.swift
+//  chakuseki-now-macUITests
+//
+//  Created by 鈴木拓也 on 2026/04/22.
+//
+
+import XCTest
+
+final class chakuseki_now_macUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
