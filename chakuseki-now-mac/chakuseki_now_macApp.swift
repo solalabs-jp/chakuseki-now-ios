08e0decf95ddd0f2bc58f5f17508ef98c26db58c
takuya <219540123+suzuki248@ users.noreply.github.com>
2026-04-23 11:23:35 +0900

    Hello World

--- chakuseki-now-mac/chakuseki_now_macApp.swift
+++ chakuseki-now-mac/chakuseki_now_macApp.swift
@@ -1,32 +1,10 @@
-//
-//  chakuseki_now_macApp.swift
-//  chakuseki-now-mac
-//
-//  Created by 鈴木拓也 on 2026/04/22.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
 struct chakuseki_now_macApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
