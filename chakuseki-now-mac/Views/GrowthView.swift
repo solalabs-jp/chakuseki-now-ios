ea0e2c56be4d4fcbe7eff4e2f4ec17b35e3a9291
takuya <219540123+suzuki248@ users.noreply.github.com>
2026-04-28 10:24:49 +0900

    画面遷移の実装

---
+++ chakuseki-now-mac/Views/GrowthView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct GrowthView: View {
+    var body: some View {
+        VStack{
+            Text("成長確認画面")
+        }
+    }
+}
+
+#Preview {
+    GrowthView()
+}
---
+++ chakuseki-now-mac/Views/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  chakuseki-now-mac
+//
+//  Created by 鈴木拓也 on 2026/04/28.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        VStack{
+            Text("履歴画面")
+        }
+    }
+}
+
+#Preview {
+    HistoryView()
+}
